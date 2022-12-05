816acec6a83e9a55b63415c71620322e6d5d7663
vatchapol jittipattanakulchai <jitti.geng@gmail.com>
2022-12-05 15:39:59 +0700

    test space

--- src/main/java/com/eaztbytes/accounts/AccountsApplication.java
+++ src/main/java/com/eaztbytes/accounts/AccountsApplication.java
@@ -18,6 +18,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 public class AccountsApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(AccountsApplication.class, args);
 	}
 
